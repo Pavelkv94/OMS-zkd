cdbec86238ad7ca7cc89786266dd8c5a9e14ed08
Pavelkv94 <pavel.kv94@yandex.by>
2022-01-30 18:17:51 +0300

    fix mobile version

---
+++ script.js
@@ -0,0 +1,21 @@
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
+
+let subMenu1 = document.querySelector('.first-submenu-mobile');
+let subItem1 = document.querySelector('.first-sub-item');
+
+subMenu1.addEventListener('click', function(){
+    subItem1.classList.toggle('active');
+});
+
+let subMenu2 = document.querySelector('.second-submenu-mobile');
+let subItem2 = document.querySelector('.second-sub-item');
+
+subMenu2.addEventListener('click', function(){
+    subItem2.classList.toggle('active');
+});
