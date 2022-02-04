2695e5262a40ddbe1dd64b57550147f6cdbe9295
Pavelkv94 <pavel.kv94@yandex.by>
2022-02-04 20:32:39 +0300

    fix bugs

--- script.js
+++ script.js
@@ -21,18 +21,4 @@ subMenu2.addEventListener('click', function(){
     subItem2.classList.toggle('active');
 });
 
-// Select Specialist
-let selectOne = document.querySelector('.select-one');
-let selectTwo = document.querySelector('.select-two');
-
-selectOne.classList.toggle('active');
-
-selectOne.addEventListener('mouseover', function(){
-	selectOne.classList.add('active');
-    selectTwo.classList.remove('active');
-})
-selectTwo.addEventListener('mouseover', function(){
-	selectTwo.classList.add('active');
-    selectOne.classList.remove('active');
-})
 
