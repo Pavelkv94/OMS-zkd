d90f3ea7a01df754c074f688317f1d4e02196275
Pavelkv94 <pavel.kv94@yandex.by>
2022-02-17 16:02:49 +0300

    reset gh

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+
+// Select Specialist
+let selectOne = document.querySelector('.select-one');
+let selectTwo = document.querySelector('.select-two');
+
+selectOne.classList.toggle('active');
+
+selectOne.addEventListener('mouseover', function(){
+	selectOne.classList.add('active');
+    selectTwo.classList.remove('active');
+})
+selectTwo.addEventListener('mouseover', function(){
+	selectTwo.classList.add('active');
+    selectOne.classList.remove('active');
+})
+
+// Slider
+// let sliderSection = document.getElementById('how-section');
+
+// if(window.innerWidth > 640) {
+//     sliderSection.classList.add('desktop')
+// } else sliderSection.classList.add('slider')
+
+$('.slider').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
