2e9a067500c5f0b43e7f832a7d6736888070cb19
Pavel Kazlou <obilonix@proton.me>
2023-06-22 13:43:19 +0300

    reinit

---
+++ pages/forStartups/forStartups.js
@@ -0,0 +1,7 @@
+$('.slider').slick({
+    dots: false,
+    arrows: true,
+    // autoplay: true
+    //     slidesToShow: 3,
+});
+
---
+++ scripts/slick-dots.js
@@ -0,0 +1,14 @@
+$('.slider').slick({
+    dots: true,
+    arrows: true,
+    // autoplay: true
+    //     slidesToShow: 3,
+});
+
+$('.simple-slider').slick({
+    dots: true,
+    arrows: true,
+    // autoplay: true
+    //     slidesToShow: 3,
+});
+
