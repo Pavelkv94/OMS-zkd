eaa78e9a9f0fcd5734f07e0db1c3fc21902e52c9
Pavel Kazlou <obilonix@proton.me>
2023-07-28 10:46:23 +0300

    reset

---
+++ scripts/contact.js
@@ -0,0 +1,37 @@
+let contactDiv1 = document.getElementById("contactmain");
+if (contactDiv1){
+contactDiv1.innerHTML = `<div class="contact-us-contacts">
+<p>Call us: <a href="tel:+48796594095">+48 796 594 095</a></p>
+<p>Email: <a href="mailto:info@zakodix.tech">info@zakodix.tech</a></p>
+<div>
+  <a href="https://t.me/Zakodix">	                                <img src="./assets/img/svg/telegram.svg" alt="telegram link"/></a>
+  <a href="https://www.linkedin.com/company/zakodix-tech-group/">	<img src="./assets/img/svg/linkedin.svg" alt="linkedIn link"/></a>
+</div>
+</div>`};
+
+
+// let contactDiv2 = document.getElementById("contactfooter");
+// if (contactDiv2){
+// contactDiv2.innerHTML = `<div class="footer-contacts">
+// <div><img src="../../assets/img/svg/logo 2.svg" alt="logo2" /></div>
+// <div>
+// <a href="mailto:info@zakodix.tech">	                            <img src="../../assets/img/svg/mail.svg" alt="Mail"/></a>
+// <a href="https://www.linkedin.com/company/zakodix-tech-group/">	<img src="../../assets/img/svg/linkedinWhite.svg" alt="linkedIn"/></a>
+// <a href="tel:+48796594095">						                          <img src="../../assets/img/svg/phone.svg" alt="Phone"/></a>
+// <a href="https://t.me/Zakodix">		                              <img src="../../assets/img/svg/telegramWhite.svg" alt="telegram"/></a>
+// </div>
+// </div>`};
+
+
+// let contactDiv5 = document.getElementById("contactfooterindex");
+// if (contactDiv5){
+// contactDiv5.innerHTML = `<div class="footer-contacts">
+// <div><img src="./assets/img/svg/logo 2.svg" alt="logo2" /></div>
+// <div>
+// <a href="mailto:info@zakodix.tech">	                            <img src="./assets/img/svg/mail.svg" alt="Mail"/></a>
+// <a href="https://www.linkedin.com/company/zakodix-tech-group/">	<img src="./assets/img/svg/linkedinWhite.svg" alt="linkedIn"/></a>
+// <a href="tel:+48796594095">						                          <img src="./assets/img/svg/phone.svg" alt="Phone"/></a>
+// <a href="https://t.me/Zakodix">		                              <img src="./assets/img/svg/telegramWhite.svg" alt="telegram"/></a>
+// </div>
+// </div>`};
+
