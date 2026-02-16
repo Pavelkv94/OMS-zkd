534906cb9db6a0267087fd56c8938422d2c6db8d
Pavel Kazlou <pnkazlou@ibagroup.eu>
2026-02-16 17:11:24 +0100

    fix

--- pages/en/ZakodixOMS/zakodixOMS.js
+++ pages/en/ZakodixOMS/zakodixOMS.js
@@ -1,6 +1,8 @@
 $('.slider').slick({
     dots: false,
     arrows: true,
+    prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><img src="../../../assets/img/svg/arrow.svg" alt="Previous"></button>',
+    nextArrow: '<button class="slick-next" aria-label="Next" type="button"><img src="../../../assets/img/svg/arrow-1.svg" alt="Next"></button>',
     // autoplay: true
     //     slidesToShow: 3,
 });
